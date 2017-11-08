28a5b6d0bb9b7fb13ccbdc0824cef526666d2aa6
yang.hao <yang.hao@igarment.net>
2017-11-08 18:06:39 +0800

    init 2

---
+++ VanillaJs/Object/Inheritance.js
@@ -0,0 +1,21 @@
+(function(){
+  const Parent = function(name){
+    this.name = name
+    this.colors = ['red', 'blue', 'yello']
+  }
+  Parent.prototype.sayName = function () {
+    console.log(this.name)
+  }
+
+  // inherit attributes  
+  const Children = function (name, age) {
+    Parent.call(this, name)
+    this.age = age
+  }
+  // inherit methods
+  Children.prototype = new Parent()
+  Children.prototype.constructor = Children
+  Children.prototype.sayAge = () => {
+    console.log(this.age)
+  }
+}())
