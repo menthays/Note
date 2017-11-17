404932daab4d8e67d6c512864304d2fdd448e782
yang.hao <yang.hao@igarment.net>
2017-11-17 16:18:34 +0800

    add gitignore & common functions in vanilla js

--- VanillaJs/Common Methods Complement/index.js
+++ VanillaJs/Common Methods Complement/index.js
@@ -2,4 +2,19 @@ const trim = (str) => {
   return str.match(/\S.*\S/)[0]
 }
 
-console.log(trim(' a b c '))
+// deep copy
+var cloneObj = function(obj){
+  var str, newobj = obj.constructor === Array ? [] : {}
+  if(typeof obj !== 'object'){
+      return
+  } else if(window.JSON){
+      str = JSON.stringify(obj) //系列化对象
+      newobj = JSON.parse(str) //还原
+  } else {
+      for(var i in obj){
+          newobj[i] = typeof obj[i] === 'object' ? 
+          cloneObj(obj[i]) : obj[i];
+      }
+  }
+  return newobj
+}
